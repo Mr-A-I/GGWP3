231bc1f097f3eb3c5fca252784d115ed1ad10830
Mr-A-I <alibabaevvasilij89@gmail.com>
2026-03-26 00:35:18 +0500

    Add files via upload

---
+++ modulFINAL-solution/js/script.js
@@ -0,0 +1,34 @@
+(function (global) {
+  var dc = {};
+  var homeHtml = "snippets/home-snippet.html";
+  var allCategoriesUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+  var menuItemsUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+
+  // Вспомогательная функция для вставки HTML
+  var insertHtml = function (selector, html) {
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
+
+  // Выбор случайной категории
+  dc.loadSpecials = function () {
+    $ajaxUtils.sendGetRequest(allCategoriesUrl, function (categories) {
+      // Генерируем случайный индекс
+      var randomIndex = Math.floor(Math.random() * categories.length);
+      // Получаем short_name случайной категории (например, "L", "D", "SP")
+      var randomCategoryShortName = categories[randomIndex].short_name;
+      
+      // Вызываем загрузку меню для этой категории
+      dc.loadMenuItems(randomCategoryShortName);
+    });
+  };
+
+  // Функция загрузки меню (заглушка для логики)
+  dc.loadMenuItems = function (categoryShortName) {
+    console.log("Loading menu for: " + categoryShortName);
+    // Здесь обычно идет переход на страницу категории
+  };
+
+  global.$dc = dc;
+
+})(window);
